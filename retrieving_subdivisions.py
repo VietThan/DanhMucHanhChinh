adb698f0e09ed9ae06a78c7a8b1697fb094c2bbf
VietThanLinux <thanhoangviet@gmail.com>
2022-06-15 23:43:00 -0400

    finished with districts

--- retrieving_subdivisions.py
+++ retrieving_subdivisions.py
@@ -110,12 +110,37 @@ def parse_province_content(province_content):
 
         province["name"] = item["TenTinh"]
         province["type"] = item["LoaiHinh"]
-        province["service_key"] = item["MaTinh"]
-        province["service_order"] = item["@msdata:rowOrder"]
+        province["province_service_key"] = item["MaTinh"]
+        province["province_service_order"] = item["@msdata:rowOrder"]
     
         provinces.append(province)
 
     return provinces
+
+def parse_district_content(district_content):
+    district_content_dict = xmltodict.parse(district_content)
+    district_dict = return_value_with_key(root=district_content_dict, key='TABLE')
+
+    districts = []
+    for item in district_dict:
+        district = {}
+
+        keys = ("MaTinh", "TenTinh", "MaQuanHuyen", "TenQuanHuyen", "LoaiHinh", "@msdata:rowOrder", "@diffgr:id")
+        for key in item:
+            if key not in keys:
+                print(f"found weird key: {key}")
+                print(f"    with values: {item[key]}")
+
+        district["province_service_key"] = item["MaTinh"]
+        district["province_name"] = item["TenTinh"]
+        district["district_service_key"] = item["MaQuanHuyen"]
+        district["name"] = item["TenQuanHuyen"]
+        district["type"] = item["LoaiHinh"]
+        district["province_service_order"] = item["@msdata:rowOrder"]
+    
+        districts.append(district)
+        
+    return districts
 
 def get_everything():
     """
@@ -124,15 +149,17 @@ def get_everything():
     into json format.
     """
     # get province
-    province_content = get_danhmuchanhchinh_response(PROVINCE_BODY, PROVINCE_HEADERS)
-    provinces = parse_province_content(province_content)
-    with open('provinces.json', 'w', encoding='utf8') as provinces_json_name:
-        json.dump(provinces, provinces_json_name, ensure_ascii=False, indent=4)
+    # province_content = get_danhmuchanhchinh_response(PROVINCE_BODY, PROVINCE_HEADERS)
+    # provinces = parse_province_content(province_content)
+    # with open('provinces.json', 'w', encoding='utf8') as provinces_json_name:
+    #     json.dump(provinces, provinces_json_name, ensure_ascii=False, indent=4)
 
     
-    # # get district
-    # district_content = get_danhmuchanhchinh_response(DISTRICT_BODY, DISTRICT_HEADERS)
-    # districts = parse_district_content(province_content)
+    # get district
+    district_content = get_danhmuchanhchinh_response(DISTRICT_BODY, DISTRICT_HEADERS)
+    districts = parse_district_content(district_content)
+    with open('districts.json', 'w', encoding='utf8') as district_json_name:
+        json.dump(districts, district_json_name, ensure_ascii=False, indent=4)
     
     # # get wards
     # ward_content = get_danhmuchanhchinh_response(WARD_BODY, WARD_HEADERS)
